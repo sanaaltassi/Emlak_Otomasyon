25bbe47432361f6b2607c1fff720d0c111719ab2
ShahdAbbara <75950493+ShahdAbbara@users.noreply.github.com>
2022-06-11 23:43:07 +0300

    Görüntüleme

---
+++ Emlak_Otomasyonu/listings/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices 
+
+from .models import Listing
+
+
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+
+    return render(request, 'listings/listings.html', context)
+
+
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
+
+
+def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+   
