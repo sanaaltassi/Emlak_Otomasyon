1f9a5c9b1fa425a09a0a79dec8000fe6541d377b
DELL <sanaalatassi2000@gmail.com>
2022-06-11 23:09:27 +0300

    admin

--- Emlak_Otomasyonu/contacts/admin.py
+++ Emlak_Otomasyonu/contacts/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'Adı', 'Listeleme', 'E-posta', 'iletişim_tarihi')
-    list_display_links = ('id', 'Adı')
-    search_fields = ('Adı', 'E-posta', 'Listeleme')
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
     list_per_page = 25
 
 admin.site.register(Contact, ContactAdmin)
