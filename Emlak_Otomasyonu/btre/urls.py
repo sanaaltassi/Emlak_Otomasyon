6d590cedef18a23a53fd13566e7fb3f066c90ef0
ShahdAbbara <75950493+ShahdAbbara@users.noreply.github.com>
2022-06-15 17:59:58 +0300

    Update urls.py

--- Emlak_Otomasyonu/btre/urls.py
+++ Emlak_Otomasyonu/btre/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
     path('accounts/', include('accounts.urls')),
     path('contacts/', include('contacts.urls')),
